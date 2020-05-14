d1015835b1aa4507f6f9593ab12f8ed1dcbab041
Jules <jujul.vivi@gmail.com>
2020-05-14 10:28:43 +0200

    ajout map

--- src/app/clic-location/clic-location.page.ts
+++ src/app/clic-location/clic-location.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
-  selector: 'app-clic-location',
-  templateUrl: './clic-location.page.html',
-  styleUrls: ['./clic-location.page.scss'],
+    selector: 'app-clic-location',
+    templateUrl: './clic-location.page.html',
+    styleUrls: ['./clic-location.page.scss'],
 })
 export class ClicLocationPage implements OnInit {
+    map: Map<any, any>;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+
+    ionViewDidEnter() { this.leafletMap(); }
+
+    leafletMap() {
+        // In setView add latLng and zoom
+        this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
+        tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'edupala.com © ionic LeafLet',
+        }).addTo(this.map);
+
+
+        marker([28.6, 77]).addTo(this.map)
+            .bindPopup('Ionic 4 <br> Leaflet.')
+            .openPopup();
+    }
+
+    /** Remove map when we have multiple map object */
+    ionViewWillLeave() {
+        this.map.remove();
+    }
 
 }
